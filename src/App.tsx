604cbf578ccaf9ae68a2260bc1fa592f72508b9c
Jonas <jonasbbernardi@gmail.com>
2024-12-15 22:57:31 -0300

    fix: using project from context

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,23 @@ import * as React from "react";
 import {
   FunctionComponent,
   useState,
-  useEffect
+  useEffect,
+  useContext
 } from 'react';
 import {
   SplitterDirection,
   SplitterElementPosition,
   Splitter
 } from "azure-devops-ui/Splitter";
-import {ProjectContext, Project} from './context/ProjectContext';
+import {ProjectContext} from './context/ProjectContext';
 import { Page } from "azure-devops-ui/Page";
 import { ObservableValue } from 'azure-devops-ui/Core/Observable';
 import { BuildFolders } from "./components/BuildFolders";
 
 export const App: FunctionComponent = () => {
     const collapsedValue = new ObservableValue(false);
+    const project = useContext(ProjectContext);
     const [collapsed, setCollapsed] = useState(false);
-    const [project, setProject] = useState(Project);
-
-    useEffect(() => {
-      setProject(Project);
-    }, [])
 
     useEffect(() => {
       collapsedValue.value = collapsed;
@@ -43,18 +40,18 @@ export const App: FunctionComponent = () => {
     return (
         <div style={containerStyle}>
             <ProjectContext.Provider value={project}>
-            <Splitter
-                collapsed={collapsed}
-                fixedElement={SplitterElementPosition.Near}
-                splitterDirection={SplitterDirection.Vertical}
-                initialFixedSize={300}
-                minFixedSize={100}
-                nearElementClassName="v-scroll-auto custom-scrollbar"
-                farElementClassName="v-scroll-auto custom-scrollbar"
-                onCollapsedChanged={collapsed => (setCollapsed(collapsed))}
-                onRenderNearElement={_renderNearContent}
-                onRenderFarElement={_renderFarContent}
-            />
+              <Splitter
+                  collapsed={collapsed}
+                  fixedElement={SplitterElementPosition.Near}
+                  splitterDirection={SplitterDirection.Vertical}
+                  initialFixedSize={300}
+                  minFixedSize={100}
+                  nearElementClassName="v-scroll-auto custom-scrollbar"
+                  farElementClassName="v-scroll-auto custom-scrollbar"
+                  onCollapsedChanged={collapsed => (setCollapsed(collapsed))}
+                  onRenderNearElement={_renderNearContent}
+                  onRenderFarElement={_renderFarContent}
+              />
             </ProjectContext.Provider>
         </div>
     )
